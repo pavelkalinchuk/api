bcbf7bda91ad2504bddbda5d0634d2ed55c22df9
pavelkalinchuk <715m4s@gmail.com>
2021-09-14 23:48:09 +0300

    Изменения для for_test.py

--- for_tests.py
+++ for_tests.py
@@ -1,5 +1,12 @@
 import requests
+import json
 
+# Получаем данные по rest api в переменную response в виде json-файла
 response = requests.get('https://reqres.in/api/users?page=2').json()
-# a = response.text
-print(response)
+# Открываем на запись файл result.json в текущем каталоге
+f = open("result.json", 'w')
+# Записываем полученные данные в открытый файл result.json отформатировав данные в читабельный вид
+f.write(json.dumps(response, indent=4))
+# Закрываем файл result.json
+f.close()
+# print(json.dumps(response, indent=4)
